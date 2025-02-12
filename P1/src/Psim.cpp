65ea790608ddd6768f7baa8850d6ad839e8425e4
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-02-11 21:26:09 -0500

    test overriding default filepaths

--- P1/src/Psim.cpp
+++ P1/src/Psim.cpp
@@ -1,40 +1,32 @@
-// Instructions: <Opcode, Destination Reg, First Op, Second Op>
-// Register File: <Reg name, Reg value>
-// datamemory: <addr, value>
-
-// tokens are memory locations
-
-// declare transitions
-// - each transition has a bool ACT flag
-// - each transition will make a copy of neighboring nodes
-// - each transition will have X number input "ports" and Y output "ports"
-// - each "port" takes a node pointer
-// - each transition may perform an action, after which the ACT flag is marked false
-// - actions on node states from copies will be committed at the end of a simulation timestep
-
-// declare nodes
-// Instruction Mememory (init)
-// - token format: <Opcode, Destination Reg, First Op, Second Op>
-// - provides instruction tokens
-// - max 16 tokens
-//
-// Register File (init)
-// - 8 registers 0-7
-// - up to 8 tokens per register
-// - value can be 0-63
-// 
-// Data Memory (init)
-// - 8 locations in memory
-// - up to 8 tokens per time step
-// - value can be 0-63
-
 #include <iostream>
+#include <string>
+
+// Global variables
+
+std::string InstructionsPath = "instructions.txt";
+std::string RegistersPath    = "registers.txt";
+std::string DataMemoryPath   = "datamemory.txt";
+std::string OutputFilePath   = "simulation.txt";
+
 // argc is # of arguments including program execution
 // argv is the array of strings of every argument including execution
 int main(int argc, char* argv[]) {
-    std::cout << "Hello World!" << std::endl;
-    std::cout << argv[1] << std::endl;
-    std::cout << argv[2] << std::endl;
-    std::cout << argv[3] << std::endl;
-    return 0;
+  std::cout << "Hello World!" << std::endl;
+
+  // change path locations if arguments set
+  if (argc >= 4) {
+    InstructionsPath = argv[1];
+    RegistersPath    = argv[2];
+    DataMemoryPath   = argv[3];
+  }
+  // optionally override output filepath if set
+  if (argc == 5) {
+    OutputFilePath = argv[4];
+  }
+    std::cout << InstructionsPath << std::endl;
+    std::cout << RegistersPath << std::endl;
+    std::cout << DataMemoryPath << std::endl;
+    std::cout << OutputFilePath << std::endl;
+
+  return 0;
 }
