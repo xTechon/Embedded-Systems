304b350bdce72237fdae66b14197c8fe34fb9ed0
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-04-01 19:23:39 -0400

    make sure correct files are not overwritten

--- P2/src/SIM.cpp
+++ P2/src/SIM.cpp
@@ -1,15 +1,25 @@
 // I have neither given nor received any unauthorized aid on this assignment
+#include <ostream>
+#include <fstream>
 #include <iostream>
+#include <iterator>
+#include <regex>
 #include <string>
+#include <vector>
 
 // can only use std on this project, so it's okay to use this
 // would not use this under normal circumstances
 using namespace std;
 
+// #beginregion --- Global Variables ---
+
 string compressionInput    = "original.txt";
 string compressionOutput   = "cout.txt";
 string decompressionInput  = "compressed.txt";
 string decompressionOutput = "dout.txt";
+vector<string> fileOutput; // contains the cycles for the file output
+
+// #endregion
 
 // argc is # of arguments including program execution
 // argv is the array of strings of every argument including execution
@@ -32,3 +42,51 @@ int main(int argc, char* argv[]) {
 
   return 0;
 }
+
+
+// generic token parsers as a helper function for the more specifc token generators
+vector<vector<string>> parser(string filePath, string delimiterRegex) {
+
+  // open the file
+  ifstream file(filePath);
+  // set the regex
+  regex del(delimiterRegex);
+
+  // create the file itterator
+  istream_iterator<string> fItterator(file);
+  istream_iterator<string> fEnd;
+
+  vector<vector<string>> output; // output variable
+
+  // itterate over the file
+  while (fItterator != fEnd) {
+
+    // split the line of the file by the regex delimiter
+    sregex_token_iterator it((*fItterator).begin(), (*fItterator).end(), del, -1);
+    sregex_token_iterator end;
+
+    // make vector to contain token
+    vector<string> parsedToken;
+
+    // itterate over the line of the file
+    while (it != end) {
+
+      // make sure to remove any empty strings
+      if (*it != "") {
+        // Add the block to the output vector
+        parsedToken.push_back(*it);
+      }
+
+      ++it;
+    } // END While
+
+    // add the token to the output
+    output.push_back(parsedToken);
+    ++fItterator;
+  } // END While
+
+  // make sure to close the file when finished
+  file.close();
+
+  return output;
+} // END parser()
